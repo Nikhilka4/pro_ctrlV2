18182c81880e9e985e486959dbecc26977b6870d
Nikhil <nikhil.medepalli27@gmail.com>
2025-04-27 12:38:48 +0530

    small change

--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -12,8 +12,8 @@ export function TeamSwitcher() {
             <GalleryVerticalEnd className="size-4" />
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">Acme Inc</span>
-            <span className="truncate text-xs">Enterprise</span>
+            <span className="truncate font-semibold">Pranavi PEB</span>
+            <span className="truncate font-semibold">Constructions</span>
           </div>
         </SidebarMenuButton>
       </SidebarMenuItem>
